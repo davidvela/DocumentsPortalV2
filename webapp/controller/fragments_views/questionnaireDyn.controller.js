977421b74cff635240ee6cf5e39f6366efb3661a
VELA TIRADO DAVID <david.vela.tirado@gmail.com>
2017-03-21 23:51:52 +0000

    test dynamic questionnaire - now I can work!

--- webapp/controller/fragments_views/questionnaireDyn.controller.js
+++ webapp/controller/fragments_views/questionnaireDyn.controller.js
@@ -10,8 +10,33 @@ sap.ui.define([
 	function(BaseController, JSONModel, History, formatter, Filter, FilterOperator, MessageToast) {
 		"use strict";
 		return BaseController.extend("portaltest.controller.fragments_views.questionnaireDyn", {
-			onInit : function () {
-				//console.log("init Collections");	
+
+			onInit: function() {
+			var screens = {
+				elements: [{
+					Id: "1",
+					Name: "Header"
+				}, {
+					Id: "2",
+					Name: "Material Scope"
+				}, {
+					Id: "3",
+					Name: "SDS"
+				}, {
+					Id: "4",
+					Name: "Formula"
+				} ]
+			};
+				//console.log("init Controller with local data for testing");	
+				//var oModel = new sap.ui.model.json.JSONModel();
+				//oModel.setData(screens);
+				var oModel = new JSONModel(jQuery.sap.getModulePath("portaltest.localService", "/questionnaire.json"));
+				//sap.ui.getCore().setModel(oModel, "screens" );
+				this.getView().setModel(oModel, "screens");
+				var oModel = new JSONModel(jQuery.sap.getModulePath("portaltest.localService", "/questionnaireDyn.json"));
+				//sap.ui.getCore().setModel(oModel, "screens" );
+				this.getView().setModel(oModel);
+
 			},
 
 			formatter: formatter,
