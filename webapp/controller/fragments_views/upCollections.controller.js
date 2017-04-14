727afce9870c4f8dca126aa3db98f5a0d8cbc87b
VELA TIRADO DAVID <david.vela.tirado@gmail.com>
2017-04-14 00:27:53 +0000

    side panel implementation - just testing

--- webapp/controller/fragments_views/upCollections.controller.js
+++ webapp/controller/fragments_views/upCollections.controller.js
@@ -25,7 +25,33 @@ sap.ui.define([
 			onPressTest4: function(oEvent) {
 				sap.m.MessageToast.show("Fragment button own controller JS");
 			},
-
+		onBeforeRendering: function() {
+			//this.getView().byId("DSCWidthSlider").setVisible(!sap.ui.Device.system.phone);
+			//this.getView().byId("DSCWidthHintText").setVisible(!sap.ui.Device.system.phone);
+		},
+		handleSliderChange: function (oEvent) {
+			var iValue = oEvent.getParameter("value");
+			this.updateControlWidth(iValue);
+		},
+		updateControlWidth: function (iValue) {
+			var $DSCContainer = this.getView().byId("sideContentContainer").$();
+			if (iValue) {
+				$DSCContainer.width(iValue + "%");
+			}
+		},
+		updateToggleButtonState: function (oEvent) {
+			var oToggleButton = this.getView().byId("toggleButton"),
+				sCurrentBreakpoint = oEvent.getParameter("currentBreakpoint");
+ 
+			if (sCurrentBreakpoint === "S") {
+				oToggleButton.setEnabled(true);
+			} else {
+				oToggleButton.setEnabled(false);
+			}
+		},
+		handleToggleClick: function () {
+			this.getView().byId("DynamicSideContent").toggle();
+		},
 			self: function() {
 				return this;
 			}
