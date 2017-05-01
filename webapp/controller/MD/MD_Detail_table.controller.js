3493cd67bd336037c261e4e7ea6d9a4168942b28
VELA TIRADO DAVID <david.vela.tirado@gmail.com>
2017-05-01 01:56:01 +0000

    table functionality: Dynamic insert and delete

--- webapp/controller/MD/MD_Detail_table.controller.js
+++ webapp/controller/MD/MD_Detail_table.controller.js
@@ -55,13 +55,70 @@ sap.ui.define([
 					}	}
 				}); */
 			this.setModel(oViewModel, "detailView");
+			this.buildDynamicScreen();
+		},
+		onPress_removeRow: function(oItem){  //console.log("Remove row");
+				var tbl = oItem.getSource().getParent().getParent();
+			    var idx = tbl.getSelectedIndex();
+		        
+		        if (idx !== -1) {
+		          var m = tbl.getModel();
+		          var data = m.getData();
+		          var removed = data.rows.splice(idx, 1);
+		          m.setData(data);
+		          sap.m.MessageToast.show(JSON.stringify(removed[0]) +  'is removed');
+		          tbl.setSelectedIndex(-1);
+		        } else {
+		          sap.m.MessageToast.show('Please select a row');
+		        }
+			
+		}	,
+		onPress_addRow: function(oItem){ 	
+			//console.log("Add row");
+			//var row = new sap.ui.table.Row({});
+			//oItem.getSource().getParent().getParent().addRow(row);
+			var oModel = oItem.getSource().getParent().getParent().getModel(); //.getProperty("/rows");
+			var oRows = oModel.getProperty("/rows");
+			
+			var example = { firstName: "jose", 	lastName: "Lal",	department: "Film"  };
+			var oRow = oModel.getProperty("/rows/0");
+			for(var i in oRow){
+				//oRow[i] = "";
+				example[i] = "";
+			}
+			
+			oRows.push(example);
+			//oRows.push(oRow);
+			oModel.setProperty("/rows", oRows);
 
 		},
 		buildDynamicScreen: function( ) {
 			var oElement = this.byId("detailContainerMDRT");
 			oElement.removeAllContent();
-				var length = rowData.length;
-					var oTable = new sap.ui.table.Table({  visibleRowCount: length  		});
+			var length = 10; // rowData.length;
+			var oTable = new sap.ui.table.Table({  
+						visibleRowCount: length ,	
+						selectionMode: sap.ui.table.SelectionMode.Single, //Single, MultiTonggle, None
+						toolbar: new sap.m.Toolbar({content : [
+																	new sap.m.Label({
+																		text : "Table title..."
+																	}),
+																	new sap.m.ToolbarSpacer(),
+																	new sap.m.Button({
+																		text : "New Row",
+																		press :this.onPress_addRow}),
+																	new sap.m.Button({
+																		text : "Remove Row",
+																		type : "Reject",
+																		press : this.onPress_removeRow
+																		/* function() {
+																			//oList.getHeaderToolbar().destroy();
+																			console.log("ola");
+																		}*/
+																	})
+																]			})  
+						
+					});
 					//var oTable = new sap.m.Table({   mob table does not have column binding!
 					//	visibleRowCount: 3 visibleRowCount: 10
 					//});
@@ -75,16 +132,17 @@ sap.ui.define([
 
 					oTable.bindColumns("/columns", function(sId, oContext) {
 						var columnName = oContext.getObject().columnName;
+						
+						var template = new sap.m.Input({value: '{' + columnName  + '}'});
+						
 						return new sap.ui.table.Column({
 							label: columnName,
-							template: columnName
+							template: template //columnName
 						});
 					});
 
 					oTable.bindRows("/rows");
 					oElement.addContent(oTable);
-
-			
 		}
 
 	});
